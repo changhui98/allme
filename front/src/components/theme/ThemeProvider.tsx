f364796364b1167a7af3426d78031010cfb8e612
changhui98 <net_0102@naver.com>
2026-08-13 17:53:39 +0900

    Feat : 자체 CSS reset 도입 및 Tailwind 완전 제거

--- front/src/components/theme/ThemeProvider.tsx
+++ front/src/components/theme/ThemeProvider.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 /**
  * next-themes 래퍼.
- * - attribute="class": <html>에 .dark 클래스를 토글 (globals.css의 @custom-variant와 연동)
+ * - attribute="class": <html>에 .dark 클래스를 토글 (수제 CSS의 `.dark .블록` 셀렉터·토큰 재정의와 연동)
  * - defaultTheme="light" + enableSystem={false}: OS 설정과 무관하게 기본은 항상 라이트
  * - disableTransitionOnChange: 테마 전환 시 색상 트랜지션이 번지는 현상 방지
  */
